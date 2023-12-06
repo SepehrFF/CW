49bf9ea3606d23360ff47f17bcac432ddd4b8d1d
Sepehr <a.sepehr83@gmail.com>
2023-12-06 14:53:01 +0330

    edited calclator

--- calculator.c
+++ calculator.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
-int calculate(int a, int b) { 
-// Some calculation return a * b; 
+#include <stdio.h> 
+int calculate(int a, int b) {
+    // A different calculation return a + b; 
 }
